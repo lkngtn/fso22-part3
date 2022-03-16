c57c3cf662fc8e4d0bc5f19aa6b85c2c4bf36aa2
lkngtn <luke@lukeduncan.net>
2022-03-16 13:35:14 -0700

    Complete exercise 3.2

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    response.end(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`);
