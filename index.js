77715a203a4739af4fcd65858875fcde865ab1e3
lkngtn <luke@lukeduncan.net>
2022-03-17 11:18:52 -0700

    Adds static frontend build

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -81,8 +82,8 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.put('/api/persons/:id', (request, response) => {
-    const updated = request.body
-    if (!updated.name || !updated.number) {
+    const body = request.body
+    if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'provide both name and number for this person'
         })
